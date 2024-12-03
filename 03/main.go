0c36d0df4dfc4312d8caa4b637149a78bd1049d7
minhtt159 <4847344+minhtt159@users.noreply.github.com>
2024-12-03 13:42:14 +0100

    feat: 03 cont

--- 03/main.go
+++ 03/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"regexp"
@@ -24,31 +25,34 @@ func read_input(file_name string) []string {
 	return result
 }
 
-func main2() {
+func main02() {
 	file_name := "real_input.txt"
 	input := read_input(file_name)
+	input = append([]string{"do()"}, input...)
 	all_input := strings.Join(input, "")
-	r := regexp.MustCompile(`do.*(mul\((\d+),(\d+)\).*)`)
+	new_input := strings.Split(all_input, "do()")
+	r := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
 
-	groups := [][]string{}
-	for _, line := range input {
-		pair := r.FindAllStringSubmatch(line, -1)
-		for _, p := range pair {
-			groups = append(groups, p)
+	result := 0
+	for _, line := range new_input {
+		index := strings.Index(line, "don't")
+		if index == -1 {
+			index = len(line)
+		}
+		do := line[:index]
+		// fmt.Println(do)
+		nice := r.FindAllStringSubmatch(do, -1)
+		for _, n := range nice {
+			// fmt.Println(n)
+			a, _ := strconv.Atoi(n[1])
+			b, _ := strconv.Atoi(n[2])
+			result += a * b
 		}
 	}
-	// fmt.Println(groups)
-	result := 0
-	for _, pair := range groups {
-		a, b := pair[1], pair[2]
-		a1, _ := strconv.Atoi(a)
-		b1, _ := strconv.Atoi(b)
-		result += a1 * b1
-	}
-	// fmt.Println(result)
+	fmt.Println(result)
 }
 
-func main() {
+func main01() {
 	file_name := "real_input.txt"
 	input := read_input(file_name)
 	r := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
@@ -68,4 +72,9 @@ func main() {
 		result += a1 * b1
 	}
 	// fmt.Println(result)
+}
+
+func main() {
+	main01()
+	main02()
 }
