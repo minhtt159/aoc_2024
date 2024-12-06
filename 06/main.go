f0ffb3394e467a27bd95607ac40f51dc7ac0ca4b
minhtt159 <4847344+minhtt159@users.noreply.github.com>
2024-12-06 18:45:18 +0100

    feat: 06'

--- 06/main.go
+++ 06/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"cmp"
 	"fmt"
 	"log"
 	"os"
@@ -11,14 +10,14 @@ import (
 )
 
 type Pair struct {
-	x, y int
+	x int
+	y int
+	d int
 }
 
-const (
-	U int = iota
-	R
-	D
-	L
+var (
+	dir_x = []int{-1, 0, 1, 0}
+	dir_y = []int{0, 1, 0, -1}
 )
 
 func read_input(file_name string) ([]string, Pair, []Pair) {
@@ -41,6 +40,7 @@ func read_input(file_name string) ([]string, Pair, []Pair) {
 			guard = Pair{
 				len(result) - 1,
 				strings.Index(line, "^"),
+				0,
 			}
 		}
 		for i, c := range line {
@@ -49,6 +49,7 @@ func read_input(file_name string) ([]string, Pair, []Pair) {
 					Pair{
 						len(result) - 1,
 						i,
+						-1,
 					})
 			}
 		}
@@ -56,139 +57,106 @@ func read_input(file_name string) ([]string, Pair, []Pair) {
 	return result, guard, obstacles
 }
 
-func fillx(input []string, orientation int, index int, from int, to int) {
-	// fmt.Println("fillx", orientation, index, from, to)
-	switch orientation {
-	case U, D:
-		for i := from; i <= to; i++ {
-			input[i] = input[i][:index] + "X" + input[i][index+1:]
-		}
-	case L, R:
-		for i := from; i <= to; i++ {
-			input[index] = input[index][:i] + "X" + input[index][i+1:]
-		}
-	default:
-		fmt.Println("Invalid orientation")
-	}
-	// for _, line := range input {
-	// 	fmt.Println(line)
-	// }
-}
-
-func letsgo(guard Pair, obstacles []Pair, TURN int, input []string) (bool, Pair) {
-	candidates := []Pair{}
-	switch TURN {
-	case U:
-		for _, o := range obstacles {
-			if o.y == guard.y && o.x < guard.x {
-				candidates = append(candidates, o)
-			}
-		}
-		if len(candidates) == 0 {
-			fillx(input, U, guard.y, 0, guard.x)
-			return false, Pair{0, 0}
-		}
-		new_guard := Pair{candidates[len(candidates)-1].x + 1, guard.y}
-		fillx(input, U, guard.y, new_guard.x, guard.x)
-		return true, new_guard
-	case D:
-		for _, o := range obstacles {
-			if o.y == guard.y && o.x > guard.x {
-				candidates = append(candidates, o)
-			}
-		}
-		if len(candidates) == 0 {
-			fillx(input, D, guard.y, guard.x, len(input)-1)
-			return false, Pair{0, 0}
-		}
-		new_guard := Pair{candidates[0].x - 1, guard.y}
-		fillx(input, D, guard.y, guard.x, new_guard.x)
-		return true, new_guard
-	case L:
-		for _, o := range obstacles {
-			if o.x == guard.x && o.y < guard.y {
-				candidates = append(candidates, o)
-			}
-		}
-		if len(candidates) == 0 {
-			fillx(input, L, guard.x, 0, guard.y)
-			return false, Pair{0, 0}
-		}
-		new_guard := Pair{guard.x, candidates[len(candidates)-1].y + 1}
-		fillx(input, L, guard.x, new_guard.y, guard.y)
-		return true, new_guard
-	case R:
-		for _, o := range obstacles {
-			if o.x == guard.x && o.y > guard.y {
-				candidates = append(candidates, o)
-			}
-		}
-		if len(candidates) == 0 {
-			fillx(input, R, guard.x, guard.y, len(input[0])-1)
-			return false, Pair{0, 0}
-		}
-		new_guard := Pair{guard.x, candidates[0].y - 1}
-		fillx(input, R, guard.x, guard.y, new_guard.y)
-		return true, new_guard
-	default:
-		return false, Pair{0, 0}
-	}
+func fillx(input []string, row int, col int, c rune) {
+	input[row] = input[row][:col] + string(c) + input[row][col+1:]
 }
 
 func main() {
-	// file_name := "input.txt"
-	file_name := "real_input.txt"
+	file_name := "input.txt"
+	// file_name := "real_input.txt"
 	input, guard, obstacles := read_input(file_name)
 	// fmt.Println(input)
-	fmt.Println(guard)
+	// fmt.Println(guard)
 	// fmt.Println(obstacles)
 
-	// Sort obstacles
-	slices.SortFunc(obstacles, func(a Pair, b Pair) int {
-		if a.x == b.x {
-			return cmp.Compare(a.y, b.y)
-		}
-		return cmp.Compare(a.x, b.x)
-	})
-	var UD []Pair
-	for _, o := range obstacles {
-		UD = append(UD, o)
-	}
-	slices.SortFunc(obstacles, func(a Pair, b Pair) int {
-		if a.y == b.y {
-			return cmp.Compare(a.x, b.x)
-		}
-		return cmp.Compare(a.y, b.y)
-	})
-	var LR []Pair
-	for _, o := range obstacles {
-		LR = append(LR, o)
-	}
-	// fmt.Println(UD)
-	// fmt.Println(LR)
-
-	TURN := U
+	TURN := 0
+	possible_obstacles := 0
 	for {
-		var canGo bool
-		switch TURN {
-		case U, D:
-			canGo, guard = letsgo(guard, UD, TURN, input)
-		case L, R:
-			canGo, guard = letsgo(guard, LR, TURN, input)
-		default: // Should not happen
-			log.Fatal("Invalid TURN")
-		}
-		fmt.Println(canGo, guard)
-		if !canGo {
+		// Try do move the guard
+		new_x := guard.x + dir_x[TURN]
+		new_y := guard.y + dir_y[TURN]
+		if new_x < 0 || new_x >= len(input) || new_y < 0 || new_y >= len(input[0]) {
+			// Guard moved out of the map, exit
 			break
 		}
-		TURN = (TURN + 1) % 4
+		if slices.Contains(obstacles, Pair{new_x, new_y, -1}) {
+			// Guard meet an obstacles, pivot
+			TURN = (TURN + 1) % 4
+			continue
+		}
+
+		// Guard can move, commit the move
+		guard = Pair{new_x, new_y, TURN}
+		fillx(input, guard.x, guard.y, 'X')
+
+		// Try to place an obstacle in the front
+		obstacles = append(obstacles,
+			Pair{
+				guard.x + dir_x[TURN],
+				guard.y + dir_y[TURN],
+				-1,
+			})
+
+		// Pivot to see if there is any loop can be achieved
+		trial_turn := (TURN + 1) % 4
+		dummy_guard := Pair{
+			guard.x,
+			guard.y,
+			guard.d,
+		}
+		dummy_stack := []Pair{dummy_guard}
+		can_place_obs := false
+
+		for {
+			forward_x := dummy_guard.x + dir_x[trial_turn]
+			forward_y := dummy_guard.y + dir_y[trial_turn]
+
+			if forward_x < 0 || forward_x >= len(input) || forward_y < 0 || forward_y >= len(input[0]) {
+				// Look ahead but out of map, break
+				break
+			}
+
+			if slices.Contains(obstacles, Pair{forward_x, forward_y, -1}) {
+				// Look ahead but hit an obstacle, pivot and continue
+				trial_turn = (trial_turn + 1) % 4
+				continue
+			}
+
+			// Try the move
+			dummy_guard = Pair{
+				forward_x,
+				forward_y,
+				trial_turn,
+			}
+			// Check if this step is already committed
+			if slices.Contains(dummy_stack, dummy_guard) {
+				can_place_obs = true
+				fmt.Println(dummy_stack)
+				fmt.Println("Loop detected, place obstacles at", guard.x+dir_x[TURN], guard.y+dir_y[TURN])
+				for _, line := range input {
+					fmt.Println(line)
+				}
+				break
+			}
+			// Commit the move
+			dummy_stack = append(dummy_stack, dummy_guard)
+		}
+		if can_place_obs {
+			possible_obstacles++
+		}
+
+		// Remove the obstacle
+		obstacles = obstacles[:len(obstacles)-1]
 	}
 
 	after_cnt := 0
 	for _, line := range input {
-		after_cnt += strings.Count(line, "X")
+		after_cnt += len(line) - strings.Count(line, "#") - strings.Count(line, ".")
 		// fmt.Println(line)
 	}
 	fmt.Println(after_cnt)
+
+	fmt.Println(possible_obstacles)
+
+	// daySix()
 }
