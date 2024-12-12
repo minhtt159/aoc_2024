e2c5f0eda9d693d776081bc5dab03f6ad10a193a
minhtt159 <4847344+minhtt159@users.noreply.github.com>
2024-12-12 20:21:35 +0100

    feat: 12 idk

--- 12/main.go
+++ 12/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"reflect"
+	"sort"
 )
 
 func read_input(file_name string) []string {
@@ -28,61 +30,71 @@ var (
 )
 
 type Plot struct {
-	id   rune
-	area int
-	peri int
+	id      rune
+	area    int
+	peri    int
+	corners int
 }
 
 func BFS(garden []string, visited map[int]map[int]bool, target rune, x int, y int) (bool, Plot) {
 	if garden[x][y] != byte(target) {
-		return false, Plot{0, 0, 0}
+		return false, Plot{0, 0, 0, 0}
 	}
 	if visited[x][y] {
-		return true, Plot{0, 0, 0}
+		return true, Plot{0, 0, 0, 0}
 	}
 	visited[x][y] = true
 	totalArea := 1
 	totalPeri := 0
+
+	// for part 2
+	totalCorners := 0
+	adj := []int{}
+
 	for k := 0; k < 4; k++ {
 		new_x := x + dir_x[k]
 		new_y := y + dir_y[k]
 		if new_x < 0 || new_x >= len(garden) || new_y < 0 || new_y >= len(garden[0]) {
-			// out of bound
+			// Out of bound
 			totalPeri++
-			continue
-		}
-		if ok, nextPlot := BFS(garden, visited, target, new_x, new_y); ok {
-			// add perimeter and area
+		} else if ok, nextPlot := BFS(garden, visited, target, new_x, new_y); ok {
+			// Neighbor plot is the same kind as this plot
 			totalArea += nextPlot.area
-			// look at how many sides are connected
-			for v := 0; v < 4; v++ {
-				next_x := new_x + dir_x[v]
-				next_y := new_y + dir_y[v]
-				if next_x < 0 || next_x >= len(garden) || next_y < 0 || next_y >= len(garden[0]) {
-					// out of bound, add perimeter
-					totalPeri++
-				} else if garden[next_x][next_y] != byte(target) {
-					// not the same plot, add perimeter
-					totalPeri++
-				} else if garden[next_x][next_y] == byte(target) {
-					// same plot, remove perimeter
-					totalPeri--
-				}
-			}
-			// inherit perimeter
 			totalPeri += nextPlot.peri
-			fmt.Println("This plot", x, y, "need", totalPeri, "fences")
+
+			// for part 2
+			totalCorners += nextPlot.corners
+			adj = append(adj, k)
+		} else {
+			// Neighbor plot is not the same kind as this plot
+			totalPeri++
 		}
 	}
 
-	fmt.Println("Visited", string(target), x, y, totalArea, totalPeri)
-	return true, Plot{target, totalArea, totalPeri}
+	// for part 2
+	sort.Ints(adj)
+	if len(adj) == 0 {
+		fmt.Println("Plot", x, y, "has no adjecent plot")
+		totalCorners += 4
+	} else if len(adj) == 1 {
+		// True corner
+		fmt.Println("Plot", x, y, "has only one adjecent plot")
+		totalCorners += 3
+	} else if reflect.DeepEqual(adj, []int{0, 2}) || reflect.DeepEqual(adj, []int{1, 3}) {
+		// L shape
+		fmt.Println("Plot", x, y, "has L shape")
+		totalCorners += 2
+	}
+
+	return true, Plot{target, totalArea, totalPeri, totalCorners}
 }
 
 func main() {
 	file_name := "input.txt"
+	// file_name := "real_input.txt"
 	input := read_input(file_name)
 	input = []string{"AAAA", "BBCD", "BBCC", "EEEC"}
+	// input = []string{"OOOOO", "OXOXO", "OOOOO", "OXOXO", "OOOOO"}
 
 	visited := make(map[int]map[int]bool)
 	for i, line := range input {
@@ -103,7 +115,10 @@ func main() {
 		}
 	}
 
+	result := 0
 	for _, plot := range allPlots {
-		log.Printf("Plot %c has area %d and perimeter %d", plot.id, plot.area, plot.peri)
+		fmt.Printf("Plot %c has area %d, perimeter %d, and corner %d\n", plot.id, plot.area, plot.peri, plot.corners)
+		result += plot.area * plot.peri
 	}
+	fmt.Println(result)
 }
